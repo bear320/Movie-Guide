087ba23183bfd5b6d105d221870b3ceb2382d62a
bear320 <aaabear320@gmail.com>
2023-02-28 16:21:56 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,264 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <section class="container">
+        <div class="search">
+            <input
+                type="text"
+                class="movie-search"
+                placeholder="Enter movie name here..."
+                ref="movie-search"
+                v-model.trim="input"
+                @keyup.enter="getMovie"
+                @input="reset"
+            />
+            <button class="search-btn" ref="search-btn" @click="getMovie">Search</button>
+        </div>
+        <div class="result" ref="result">
+            <!-- Input is empty -->
+            <template v-if="this.input.length <= 0">
+                <h3 class="msg">Please enter a movie name</h3>
+            </template>
+
+            <!-- Get movie data -->
+            <template v-else-if="this.input.length != 0 && this.isDataGet === 'get'">
+                <div class="info">
+                    <img :src="this.data.Poster" class="poster" alt="Poster" />
+                    <div>
+                        <h2>{{ this.data.Title }}</h2>
+                        <div class="rating">
+                            <img src="@/assets/images/star-icon.svg" alt="Rating" />
+                            <h4>{{ this.data.imdbRating }}</h4>
+                        </div>
+                        <div class="details">
+                            <span>{{ this.data.Year }}</span>
+                            <span>{{ this.data.Rated }}</span>
+                            <span>{{ this.data.Runtime }}</span>
+                        </div>
+                        <div class="genre">
+                            <div v-if="this.gnere1">{{ this.gnere1 }}</div>
+                            <div v-if="this.gnere2">{{ this.gnere2 }}</div>
+                            <div v-if="this.gnere3">{{ this.gnere3 }}</div>
+                        </div>
+                    </div>
+                </div>
+                <h3>Plot:</h3>
+                <p>{{ this.data.Plot }}</p>
+                <h3>Cast:</h3>
+                <p>{{ this.data.Actors }}</p>
+            </template>
+
+            <!-- Movie doesn't exist -->
+            <template v-else-if="this.isDataGet === 'error'">
+                <h3 class="msg">{{ this.data.Error }}</h3>
+            </template>
+
+            <!-- Other situations -->
+            <template v-else>
+                <h3 class="msg">Please enter a movie name</h3>
+            </template>
+        </div>
+    </section>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            input: "",
+            isDataGet: "",
+            data: null,
+            gnere1: "",
+            gnere2: "",
+            gnere3: "",
+        };
+    },
+    methods: {
+        async getMovie() {
+            if (!this.input) return;
+
+            let apiURL = `http://www.omdbapi.com/?t=${this.input}&apikey=${process.env.VUE_APP_OMDBKEY}`;
+
+            await fetch(apiURL)
+                .then((res) => res.json())
+                .then((data) => {
+                    if (data.Response == "True") {
+                        this.data = data;
+                        this.gnere1 = data.Genre.split(",")[0];
+                        this.gnere2 = data.Genre.split(",")[1];
+                        this.gnere3 = data.Genre.split(",")[2];
+                        this.isDataGet = "get";
+                    } else if (data.Response == "False") {
+                        this.data = data;
+                        this.isDataGet = "error";
+                    }
+                })
+                .catch(() => {
+                    this.$refs["result"].innerHTML = `<h3 class="msg">Error Occured</h3>`;
+                });
+        },
+        reset() {
+            this.isDataGet = "";
+            this.data = null;
+            this.gnere1 = "";
+            this.gnere2 = "";
+            this.gnere3 = "";
+        },
+    },
+    created() {
+        this.getMovie();
+    },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+body {
+    height: 100vh;
+    background: linear-gradient(#1c1917 50%, #ffb92a 50%);
+    #app {
+        font-size: 1rem;
 
-nav {
-  padding: 30px;
+        .container {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 90vw;
+            max-width: 37.5rem;
+            padding: 3rem 1.8rem;
+            background-color: #1e293b;
+            border-radius: 0.6rem;
+            box-shadow: 1.2rem 2rem 3rem #0002;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+            .search {
+                display: grid;
+                grid-template-columns: 9fr 3fr;
+                gap: 1.2rem;
 
-    &.router-link-exact-active {
-      color: #42b983;
+                .movie-search,
+                .search-btn {
+                    font-size: 0.9rem;
+                    outline: none;
+                    border-radius: 0.3rem;
+                }
+
+                .movie-search {
+                    background-color: #fff;
+                    border: 1px solid #a0a0a0;
+                    // color: #fff;
+                    padding: 0.7rem;
+
+                    &:focus {
+                        border-color: #fff;
+                    }
+                }
+
+                .search-btn {
+                    background-color: #ffb92a;
+                    border: none;
+                    cursor: pointer;
+                }
+            }
+
+            .result {
+                color: #fff;
+
+                .info {
+                    position: relative;
+                    display: grid;
+                    grid-template-columns: 4fr 8fr;
+                    margin-top: 1.2rem;
+                    overflow: scroll;
+
+                    .poster {
+                        width: 100%;
+                    }
+
+                    h2 {
+                        text-align: center;
+                        font-size: 1.5rem;
+                        font-weight: 600;
+                        letter-spacing: 0.06rem;
+                    }
+
+                    .rating {
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        gap: 0.6rem;
+                        margin: 0.9rem 0;
+
+                        img {
+                            width: 1.2rem;
+                        }
+
+                        h4 {
+                            display: inline-block;
+                            font-size: 1.1rem;
+                            font-weight: 500;
+                        }
+                    }
+
+                    .details {
+                        display: flex;
+                        justify-content: center;
+                        gap: 1rem;
+                        margin: 0.9rem 0 1.5rem;
+                        font-size: 0.95rem;
+                        font-weight: 300;
+                        color: #a0a0a0;
+                    }
+
+                    .genre {
+                        display: flex;
+                        justify-content: center;
+                        column-gap: 1rem;
+
+                        div {
+                            border: 1px solid #a0a0a0;
+                            border-radius: 0.4rem;
+                            padding: 0.4rem 1rem;
+                            font-size: 0.75rem;
+                            font-weight: 300;
+                        }
+                    }
+                }
+
+                h3 {
+                    font-weight: 500;
+                    margin-top: 1.2rem;
+                }
+
+                p {
+                    font-size: 0.9rem;
+                    font-weight: 300;
+                    line-height: 1.8rem;
+                    text-align: justify;
+                    color: #a0a0a0;
+                }
+
+                .msg {
+                    text-align: center;
+                    margin-bottom: 0;
+                }
+            }
+        }
     }
-  }
+
+    @media screen and (max-width: 600px) {
+        .container {
+            font-size: 14px;
+
+            .result {
+                .info {
+                    grid-template-columns: 1fr;
+
+                    .poster {
+                        margin: auto;
+                        width: auto;
+                        max-height: 10.8rem;
+                    }
+                }
+            }
+        }
+    }
 }
 </style>
